31d209a67705144cec0d757270753d7064889dfa
Misaki Shioi <shioi.mm@gmail.com>
2022-06-06 18:20:49 +0900

    Add class WSDissector by Ruby

---
+++ plugins/epan/foo/ws_dissector.rb
@@ -0,0 +1,29 @@
+class WSDissector
+  ENC_BIG_ENDIAN  = nil # Implement in ws_protocol.c
+  FORMAT_ADD_ITEM = :format_add_item
+
+  def initialize(name:, depth:)
+    @name     = name
+    @depth    = depth
+    @items    = []
+    @subtrees = []
+  end
+
+  def items(dissecting_items)
+    @items = dissecting_items
+  end
+
+  def sub(name, &block)
+    subtree = self.class.new(name: name, depth: @depth + 1)
+    subtree.instance_eval(&block)
+    @subtrees << subtree
+  end
+
+  def max_depth
+    if @subtrees.empty?
+      0
+    else
+      @subtrees.map(&:max_depth).max + 1
+    end
+  end
+end
